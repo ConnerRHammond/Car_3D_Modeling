385e6fd74bb98d2c09553acf0a55ff8dbe27e599
Conner Hammond <reuelerman@gmail.com>
2021-10-18 17:49:25 -0600

    Adding Car modeling Program

---
+++ Car_3D_ MODEL.py
@@ -0,0 +1,164 @@
+xaxis= 0
+yaxis= 0
+zaxis = -15
+rangle = 0
+perspective = True
+import sys
+
+try:
+    from OpenGL.GLUT import *
+    from OpenGL.GL import *
+    from OpenGL.GLU import *
+    from OpenGL.GL import glOrtho
+    from OpenGL.GLU import gluPerspective
+    from OpenGL.GL import glRotated
+    from OpenGL.GL import glTranslated
+    from OpenGL.GL import glLoadIdentity
+    from OpenGL.GL import glMatrixMode
+    from OpenGL.GL import GL_MODELVIEW
+    from OpenGL.GL import GL_PROJECTION
+except:
+    print("ERROR: PyOpenGL not installed properly. ")
+
+DISPLAY_WIDTH = 500.0
+DISPLAY_HEIGHT = 500.0
+
+
+def init(): 
+
+    glClearColor (0.0, 0.0, 0.0, 0.0)
+    glShadeModel (GL_FLAT)
+
+def drawHouse ():
+    glLineWidth(2.5)
+    glColor3f(1.0, 0.0, 0.0)
+    #Floor
+    glBegin(GL_LINES)
+    glVertex3f(-5.0, 0.0, -5.0)
+    glVertex3f(5, 0, -5)
+    glVertex3f(5, 0, -5)
+    glVertex3f(5, 0, 5)
+    glVertex3f(5, 0, 5)
+    glVertex3f(-5, 0, 5)
+    glVertex3f(-5, 0, 5)
+    glVertex3f(-5, 0, -5)
+    #Ceiling
+    glVertex3f(-5, 5, -5)
+    glVertex3f(5, 5, -5)
+    glVertex3f(5, 5, -5)
+    glVertex3f(5, 5, 5)
+    glVertex3f(5, 5, 5)
+    glVertex3f(-5, 5, 5)
+    glVertex3f(-5, 5, 5)
+    glVertex3f(-5, 5, -5)
+    #Walls
+    glVertex3f(-5, 0, -5)
+    glVertex3f(-5, 5, -5)
+    glVertex3f(5, 0, -5)
+    glVertex3f(5, 5, -5)
+    glVertex3f(5, 0, 5)
+    glVertex3f(5, 5, 5)
+    glVertex3f(-5, 0, 5)
+    glVertex3f(-5, 5, 5)
+    #Door
+    glVertex3f(-1, 0, 5)
+    glVertex3f(-1, 3, 5)
+    glVertex3f(-1, 3, 5)
+    glVertex3f(1, 3, 5)
+    glVertex3f(1, 3, 5)
+    glVertex3f(1, 0, 5)
+    #Roof
+    glVertex3f(-5, 5, -5)
+    glVertex3f(0, 8, -5)
+    glVertex3f(0, 8, -5)
+    glVertex3f(5, 5, -5)
+    glVertex3f(-5, 5, 5)
+    glVertex3f(0, 8, 5)
+    glVertex3f(0, 8, 5)
+    glVertex3f(5, 5, 5)
+    glVertex3f(0, 8, 5)
+    glVertex3f(0, 8, -5)
+    glEnd()
+
+def display():
+    glClear (GL_COLOR_BUFFER_BIT)
+    glColor3f (1.0, 1.0, 1.0)
+    # viewing transformation 
+    
+    glLoadIdentity()
+    if perspective :
+        gluPerspective(90,1,.1,100)
+    else :
+        glOrtho(-25.0, 25.0, -25.0, 25.0, -25, 25)
+    glTranslated(xaxis,yaxis,zaxis)
+    glRotated(rangle,0,1,0)
+    #Your Code Here
+    
+    drawHouse()
+
+    
+    glFlush()
+    
+
+def keyboard(key, x, y):
+    global xaxis
+    global yaxis
+    global zaxis
+    global rangle 
+    global perspective
+    if key == chr(27):
+        import sys
+        sys.exit(0)
+  
+    if key == b'w':
+        zaxis +=  1 
+    
+    if key == b'a':
+        xaxis +=  1 
+    
+    if key == b'd':
+        xaxis -=  1 
+        
+    if key == b's':
+        zaxis -=  1 
+    
+    if key == b'q':
+        rangle += 1
+    
+    if key == b'e':
+        rangle -= 1
+    
+    if key == b'r':
+        yaxis -= 1
+
+    
+    if key == b'f':
+        yaxis += 1
+    
+    if key == b'h':
+        xaxis = 0
+        yaxis = 0
+        zaxis = -15
+        rangle = 0
+    
+    if key == b'o':
+        perspective = False
+   
+    if key == b'p':
+        perspective = True
+
+  
+    #Your Code Here
+  
+    glutPostRedisplay()
+
+
+glutInit(sys.argv)
+glutInitDisplayMode (GLUT_SINGLE | GLUT_RGB)
+glutInitWindowSize (int(DISPLAY_WIDTH), int(DISPLAY_HEIGHT))
+glutInitWindowPosition (100, 100)
+glutCreateWindow (b'OpenGL Lab')
+init ()
+glutDisplayFunc(display)
+glutKeyboardFunc(keyboard)
+glutMainLoop()
